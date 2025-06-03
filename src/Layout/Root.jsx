6fb74a74b16631a14e753e777ee2fed38ba14f10
islamemon59 <islamemon59@gmail.com>
2025-06-03 22:03:01 +0600

    navbar added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../Pages/Navbar';
 
 const Root = () => {
     return (
         <div>
+            <Navbar></Navbar>
             This is root
             <Outlet></Outlet>
         </div>
