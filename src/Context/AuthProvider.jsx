8b2ec5291d8b6915d414144f192320dcf7397806
islamemon59 <islamemon59@gmail.com>
2025-06-03 23:37:41 +0600

    user login logout method added

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,22 +1,61 @@
-import React, { useState } from 'react';
-import { AuthContext } from './AuthContext';
+import React, { useEffect, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../Firebase/Firebase.config";
 
-const AuthProvider = ({children}) => {
+const googleProvider = new GoogleAuthProvider();
 
-    const [user, setUser] = useState(null)
-    const [loading, setLoading] = useState(true)
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const userInfo = {
-        user,
-        setUser,
-        loading,
-        setLoading,
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const signOutUser = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser)
+        setLoading(false)
+    })
+    return () => {
+        unSubscribe
     }
-    return (
-        <AuthContext value={userInfo}>
-            {children}
-        </AuthContext>
-    );
+  })
+
+  const userInfo = {
+    user,
+    setUser,
+    loading,
+    setLoading,
+    createUser,
+    signInUser,
+    signInWithGoogle,
+    signOutUser,
+  };
+  return <AuthContext value={userInfo}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
