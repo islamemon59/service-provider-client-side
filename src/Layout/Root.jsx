f47c16cb05df53d16bd2fe452761e9104a5eee98
islamemon59 <islamemon59@gmail.com>
2025-06-03 22:16:32 +0600

    footer page added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../Pages/Navbar';
+import Footer from '../Pages/Footer';
 
 const Root = () => {
     return (
@@ -8,6 +9,7 @@ const Root = () => {
             <Navbar></Navbar>
             This is root
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../Layout/Root";
 import Home from "../Pages/Home";
+import SignIn from "../Pages/SignIn";
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
             {
                 index: true,
                 Component: Home
+            },
+            {
+                path: '/signIn',
+                Component: SignIn
             }
         ]
     }
