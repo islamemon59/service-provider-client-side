e4b786b58b49df7edbdc634aec215497b4ed92aa
islamemon59 <islamemon59@gmail.com>
2025-06-03 22:29:51 +0600

    daysiui theme added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-import Navbar from '../Pages/Navbar';
-import Footer from '../Pages/Footer';
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../Pages/Navbar";
+import Footer from "../Pages/Footer";
 
 const Root = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            This is root
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="min-h-[calc(100vh-90px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
