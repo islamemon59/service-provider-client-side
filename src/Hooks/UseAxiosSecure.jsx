f622d5402f146605050e101c7fbfcac73cbc9280
islamemon59 <islamemon59@gmail.com>
2025-06-12 08:32:47 +0600

    readme file live url added

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -1,36 +1,38 @@
-import axios from 'axios';
-import ContextHook from './ContextHook';
-import toast from 'react-hot-toast';
+import axios from "axios";
+import ContextHook from "./ContextHook";
+import toast from "react-hot-toast";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/'
-})
+  baseURL: "http://localhost:3000/",
+});
 
 const UseAxiosSecure = () => {
+  const { user, signOutUser } = ContextHook();
 
-    const {user, signOutUser} = ContextHook()
+  axiosInstance.interceptors.request.use((config) => {
+    config.headers.authorization = `Bearer ${user?.accessToken}`;
+    return config;
+  });
 
-    axiosInstance.interceptors.request.use(config => {
-        config.headers.authorization = `Bearer ${user?.accessToken}`
-        return config
-    })
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error.status === 401 || error.status === 403) {
+        signOutUser()
+          .then(() => {
+            toast.error("unauthorized / forbidden access");
+          })
+          .catch((err) => {
+            toast.error(err.message);
+          });
+      }
+      return Promise.reject(error);
+    }
+  );
 
-    axiosInstance.interceptors.response.use(response => {
-        return response
-    }, error => {
-
-        if(error.status === 401 || error.status === 403){
-            signOutUser()
-            .then(() => {
-                toast.error('unauthorized / forbidden access')
-            }).catch(err => {
-                toast.error(err.message)
-            })
-        }
-        return Promise.reject(error)
-    })
-
-    return axiosInstance
+  return axiosInstance;
 };
 
 export default UseAxiosSecure;
